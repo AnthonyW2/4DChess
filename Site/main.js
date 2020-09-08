4abc842a7030578432f1a22641d9d251a4b5533b
Anthony Wilson <awilsonprogramming@gmail.com>
2020-09-08 14:08:44 +1000

    Preparation for Release 1.0

--- Site/main.js
+++ Site/main.js
@@ -74,6 +74,21 @@ function VersusBtnCPU(){
   document.getElementById("VersusSubmenuCPU").hidden = false;
 }
 
+
+//Create a random game ID for a new game
+function RandomiseGameID(){
+  var randID = "";
+  
+  for(var a = 0;a < 8;a += 1){
+    //Generate a random alphanumeric character and add it to the "randID" string
+    var character = Math.floor(Math.random()*62);
+    randID += String.fromCharCode(character+48+((character > 9)?(7+((character > 35)?6:0)):0));
+  }
+  
+  document.getElementById("CreateOnlineCode").value = randID;
+}
+
+
 //Create a new local game
 function NewLocalGame(){
   var LayoutSelector = document.getElementById("LocalLayoutSelector");
@@ -93,23 +108,37 @@ function JoinOnlineGame(){
       PlayerColor = a;
     }
   }
-  var GameID = document.getElementById("JoinOnlineCode").value;
   
+  var GameID = document.getElementById("JoinOnlineCode").value;
   if(GameID == "00000000" || GameID == "" || GameID == undefined){
     document.getElementById("JoinOnlineStatus").innerHTML = "Illegal game ID, please supply a new one";
   }
-  //This synchronous XMLHttpRequest will give a deprecation warning, but should work fine
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","Server/getLiveGameFromID.php?id="+GameID,false);
-  xmlhttp.send();
   
-  //Check the result from the server, and report it to the user through a clear message
-  if(xmlhttp.responseText == 0){
-    document.getElementById("JoinOnlineStatus").innerHTML = "Joining online game...";
-    window.location.href = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
-  }else{
-    document.getElementById("JoinOnlineStatus").innerHTML = "Illegal game ID, please supply a new one";
+  var Password = document.getElementById("JoinOnlinePassword").value;
+  
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var serverResult = xmlhttp.responseText;
+      
+      //Check the result from the server, and report it to the user through a clear message
+      if(parseInt(xmlhttp.responseText) == 0){
+        document.getElementById("JoinOnlineStatus").innerHTML = "Joining online game...";
+        if(Password == "" || Password == undefined){
+          window.location.href = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
+        }else{
+          //Yes, I know I'm submitting the password as plaintext, but for a game I don't believe that really matters too much
+          document.getElementById("JoinOnlineForm").action = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
+          document.getElementById("JoinOnlineForm").submit();
+        }
+      }else{
+        document.getElementById("JoinOnlineStatus").innerHTML = "Illegal game ID, please supply a new one";
+      }
+    }
   }
+  
+  xmlhttp.open("GET","Server/confirmGameExists.php?id="+GameID,true);
+  xmlhttp.send();
 }
 
 //Create a new online game
@@ -123,35 +152,56 @@ function CreateOnlineGame(){
       PlayerColor = a;
     }
   }
+  
   var LayoutSelector = document.getElementById("CreateOnlineLayoutSelector");
   var ChosenLayout = LayoutSelector.options[LayoutSelector.selectedIndex].value;
+  
   var GameID = document.getElementById("CreateOnlineCode").value;
   if(GameID == "" || GameID == undefined){
     document.getElementById("CreateOnlineStatus").innerHTML = "Please supply a game ID";
   }
   
+  var Password = document.getElementById("CreateOnlinePassword").value;
+  
   //This synchronous XMLHttpRequest will give a deprecation warning, but should work fine
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","Server/newGame.php?l="+ChosenLayout+"&id="+GameID,false);
-  xmlhttp.send();
-  var serverResult = xmlhttp.responseText;
+  xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var serverResult = xmlhttp.responseText;
+      
+      //Check the result from the server, and report it to the user through a clear message
+      switch(parseInt(serverResult)){
+        case 0:
+          document.getElementById("CreateOnlineStatus").innerHTML = "Successfully created new game, redirecting...";
+          if(Password == "" || Password == undefined){
+            window.location.href = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
+          }else{
+            //Yes, I know I'm submitting the password as plaintext, but for a game I don't believe that really matters too much
+            document.getElementById("CreateOnlineForm").action = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
+            document.getElementById("CreateOnlineForm").submit();
+          }
+          break;
+        case 1:
+          document.getElementById("CreateOnlineStatus").innerHTML = "Illegal game ID, please supply a new one";
+          break;
+        case 2:
+          document.getElementById("CreateOnlineStatus").innerHTML = "Game ID already in use, please supply different one";
+          break;
+        case 3:
+          document.getElementById("CreateOnlineStatus").innerHTML = "Server-side write error, please contact the system admin";
+          break;
+        default:
+          document.getElementById("CreateOnlineStatus").innerHTML = "An unknown error occured, please try again, or contact the system admin";
+      }
+    }
+  }
   
-  //Check the result from the server, and report it to the user through a clear message
-  switch(parseInt(serverResult)){
-    case 0:
-      document.getElementById("CreateOnlineStatus").innerHTML = "Successfully created new game, redirecting...";
-      window.location.href = "Play/?vs=1&id="+GameID+"&c="+PlayerColor;
-      break;
-    case 1:
-      document.getElementById("CreateOnlineStatus").innerHTML = "Illegal game ID, please supply a new one";
-      break;
-    case 2:
-      document.getElementById("CreateOnlineStatus").innerHTML = "Game ID already in use, please supply different one";
-      break;
-    case 3:
-      document.getElementById("CreateOnlineStatus").innerHTML = "Server-side write error, please contact the system admin";
-      break;
-    default:
-      document.getElementById("CreateOnlineStatus").innerHTML = "An unknown error occured, please try again, or contact the system admin";
+  if(Password == "" || Password == undefined){
+    xmlhttp.open("GET","Server/newGame.php?l="+ChosenLayout+"&id="+GameID,true);
+    xmlhttp.send();
+  }else{
+    xmlhttp.open("POST","Server/newGame.php?l="+ChosenLayout+"&id="+GameID,true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("passw="+Password);
   }
 }
